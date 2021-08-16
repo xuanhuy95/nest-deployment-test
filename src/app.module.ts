97b440682fdd081d8d4ec6238efc00a5776356db
xuanhuy <huynguyen@asimtelecom.vn>
2021-08-16 16:15:23 +0700

    commit message

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           password: process.env.DB_PASSWORD,
           database: process.env.DB_NAME,
           entities: [],
-          synchronize: true,
+          synchronize: false,
       }),
 
   ],
