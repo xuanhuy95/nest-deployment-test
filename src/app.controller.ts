ce3d55aa1f35f1f5c65e250e55ade25e4df4a4aa
xuanhuy <huynguyen@asimtelecom.vn>
2021-08-17 14:30:00 +0700

    Add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { CreateActivationRequestDto } from './ticket/dto/activation.dto';
 
 @Controller()
 export class AppController {
-  constructor(
-      private readonly appService: AppService,
-      private readonly configService: ConfigService,
-  ) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly configService: ConfigService,
+    ) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get('hello')
-  getHello2(): string {
-    console.log('Hello', this.configService.get('DB_USER'));
-    return this.configService.get('DB_USER');
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('hello')
+    getHello2(): string {
+        console.log('Hello', this.configService.get('DB_USER'));
+        return this.configService.get('DB_USER');
+    }
+
+    @Post('hello')
+    getHelloPost(
+        @Body() createActivationRequestDto: CreateActivationRequestDto,
+    ): string {
+        console.log('Hello', this.configService.get('DB_USER'));
+        return this.configService.get('DB_USER');
+    }
 }
